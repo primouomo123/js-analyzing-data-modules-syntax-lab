299d8f1e2987d1ce90eaec11fd8e2be6de7e5b7d
primouomo123 <luismanuelrios123@gmail.com>
2025-07-20 00:41:41 -0400

    I've finished and added eslint

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
+require('datejs');
 
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
+
+  for (const array of args) {
+    combinedObject.users.push(...array);
+  }
+
+  const today = new Date().toString("M/d/yyyy");
+
+  combinedObject.merge_date = today;
+
+  return combinedObject;
+}
 
 
 module.exports = {
